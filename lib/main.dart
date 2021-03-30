4c2f14b370d7569e2f393c564302a07f47525b82
willianfrancisco <willian.francisco140@gmail.com>
2021-03-30 20:41:30 -0300

    Criação da Função BuildItems e intens removiveis

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,10 @@ class _HomeState extends State<Home> {
   List _toDoList = [];
 
   @override
-  Void initState(){
+  Void initState() {
     super.initState();
 
-    _readData().then((data){
+    _readData().then((data) {
       setState(() {
         _toDoList = json.decode(data);
       });
@@ -77,23 +77,38 @@ class _HomeState extends State<Home> {
               child: ListView.builder(
                   padding: EdgeInsets.only(top: 10.0),
                   itemCount: _toDoList.length,
-                  itemBuilder: (context, index) {
-                    return CheckboxListTile(
-                      title: Text(_toDoList[index]["title"]),
-                      value: _toDoList[index]["Ok"],
-                      secondary: CircleAvatar(
-                        child: Icon(
-                            _toDoList[index]["Ok"] ? Icons.check : Icons.error),
-                      ),
-                      onChanged: (c){
-                        setState(() {
-                          _toDoList[index]["Ok"] = c;
-                          _saveData();
-                        });
-                      },
-                    );
-                  }))
+                  itemBuilder: buildItem))
         ],
+      ),
+    );
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["Ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["Ok"] ? Icons.check : Icons.error),
+        ),
+        onChanged: (c) {
+          setState(() {
+            _toDoList[index]["Ok"] = c;
+            _saveData();
+          });
+        },
       ),
     );
   }
